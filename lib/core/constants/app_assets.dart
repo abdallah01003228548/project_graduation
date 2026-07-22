38934c0552ed6258cd64f248a643a2d3dc71b025
shady-2006 <shady@github.com>
2026-07-22 17:18:12 +0300

    feat: configure native splash screens  for Android, iOS

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,31 @@
+﻿abstract class AppImages {
+  static const String _baseImages = 'assets/images/';
+
+  // Onboarding
+  // static const String onboarding1 = '${_baseImages}onboarding1.png';
+  // static const String onboarding2 = '${_baseImages}onboarding2.png';
+  // static const String onboarding3 = '${_baseImages}onboarding3.png';
+
+  // Auth
+  // static const String logo = '${_baseImages}logo.png';
+
+  // Placeholders
+  // static const String placeholder = '${_baseImages}placeholder.png';
+  // static const String emptyState  = '${_baseImages}empty_state.png';
+  // static const String errorImage  = '${_baseImages}error_image.png';
+}
+
+abstract class AppIconsAssets {
+  static const String _baseIcons = 'assets/icons/';
+
+  // SVG Icons (if using flutter_svg)
+  // static const String homeIcon       = '${_baseIcons}home.svg';
+  // static const String cartIcon       = '${_baseIcons}cart.svg';
+  // static const String favoriteIcon   = '${_baseIcons}favorite.svg';
+  // static const String profileIcon    = '${_baseIcons}profile.svg';
+  // static const String searchIcon     = '${_baseIcons}search.svg';
+  // static const String filterIcon     = '${_baseIcons}filter.svg';
+  // static const String backArrowIcon  = '${_baseIcons}back_arrow.svg';
+  // static const String starIcon       = '${_baseIcons}star.svg';
+  // static const String checkIcon      = '${_baseIcons}check.svg';
+}
