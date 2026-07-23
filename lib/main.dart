fa732dad90ac01b7e79cc2cb7d3c3c18d2906e0f
shady-2006 <shady@github.com>
2026-07-23 18:00:06 +0300

    feat: implement  task  5     hello screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'core/theme/app_theme.dart';
+import 'core/constants/routes.dart';
+import 'feature/hello/presentation/screens/hello_screen.dart';
+import 'feature/auth/presentation/screens/login_screen.dart';
+import 'feature/auth/presentation/screens/register_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,9 +12,21 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: 'Graduation Project',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+
+
+      initialRoute: Routes.hello,
+      routes: {
+        Routes.hello: (context) =>  const HelloScreen(),
+        Routes.login: (context) => const LoginScreen(),
+        Routes.register: (context)  => const RegisterScreen(),
+      },
+    );
   }
 }
+
