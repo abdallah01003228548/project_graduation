d3e22766d5accc55c9ddf4a0c2ddcbba9804506d
malaksamy <malaksamyii510@gmail.com>
2026-07-28 22:00:28 +0300

    Remove debug print statements

--- lib/feature/home/data/models/data_source/home_remote_data_source_imp.dart
+++ lib/feature/home/data/models/data_source/home_remote_data_source_imp.dart
@@ -58,8 +58,7 @@ class HomeRemoteDataSourceImp implements HomeRemoteDataSource {
   Future<ResultApi<List<CategoryItemDto>>> getCategories() async {
     try {
       final response = await networkModule.get('/home/categories');
-      print(response.data);
-      print(response.data.runtimeType);
+
       final List<dynamic> list = response.data['list'];
 
       final categories = list
